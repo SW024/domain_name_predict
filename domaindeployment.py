8428d461147d0485573bcd7cc5014cae112748ea
SW024 <107823636+SW024@users.noreply.github.com>
2024-04-09 22:00:58 +0800

    Update domaindeployment.py

--- domaindeployment.py
+++ domaindeployment.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import numpy as np
 
 # Load your trained model
 model = joblib.load('domain_rf_model.joblib')
 
 # Define the structure of your web app
-st.title('Domain Name Scoring System')
+st.title('Random Forest Model Deployment')
 
 # File uploader allows user to add their own CSV
 uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
@@ -14,9 +15,9 @@ uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
 if uploaded_file is not None:
     # Read the uploaded data
     data = pd.read_csv(uploaded_file)
-    st.write('Data Successfully Uploaded!')
+    st.success('Data Successfully Uploaded!')
     
-    # Assuming there's a column in the CSV called 'Domain' which contains the domain names
+    # Check if the 'Name' column is in the CSV
     if 'Name' in data.columns:
         # Let the user select a domain to predict
         selected_domain = st.selectbox('Select a Domain to Predict', data['Name'].unique())
@@ -24,21 +25,25 @@ if uploaded_file is not None:
         # Get the features of the selected domain
         domain_features = data[data['Name'] == selected_domain][['Age of Domain', 'TLD Score', 'Search Queries Occurrences', 'Length Score', 'Word Composition']]
         
-        # Display the features
+        # Display the features in separate columns
         if not domain_features.empty:
             st.write('Features of the selected domain:')
-            st.json(domain_features.iloc[0].to_json())  # Convert to JSON for pretty printing
+            st.table(domain_features.reset_index(drop=True))  # Display features in a table
 
             # Button to make prediction
             if st.button('Predict Domain Price'):
                 # Reshape the data for prediction
                 prediction_features = domain_features.values.reshape(1, -1)
                 # Predict the price
-                prediction = model.predict(prediction_features)
+                log_price_prediction = model.predict(prediction_features)
+                
+                # Convert the log price prediction back to original scale
+                # Assuming you used np.log() to get the log_price during training, you'd use np.exp() to reverse it
+                original_price_prediction = np.exp(log_price_prediction)
                 
                 # Display the prediction
-                st.write(f'The predicted log price for "{selected_domain}" is: {prediction[0]}')
+                st.write(f'The predicted price for "{selected_domain}" is: ${original_price_prediction[0]:,.2f}')
         else:
             st.error('No features found for the selected domain.')
     else:
-        st.error('CSV must have a "Domain" column.')
+        st.error('CSV must have a Domain column.')
